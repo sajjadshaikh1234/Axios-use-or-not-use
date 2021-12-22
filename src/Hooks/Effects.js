fc3219c53fe07ec059f1f15d2c4912f1db8bba36
github_sajjad <sajjadshaikh2805@gmail.com>
2021-12-22 19:22:11 +0530

    css

--- src/Hooks/Effects.js
+++ src/Hooks/Effects.js
@@ -35,56 +35,55 @@ const Effects = () => {
     }
 
         , [])
-const selector  = 
-    data.map((item,id) => {
-        console.log("item..",item.address
-        )
-        return (
-            <>
-            <tr>
-            <th scope="row"> {item.id}  </th>
-                <td>  {item.email}</td>
-                <td> {item.password}</td>
-                <td> {item.username}</td>
-                <td>{item.address.city} </td>
-                <td> {item.address.number}</td>
-                <td> {item.address.geolocation.lat}</td>
-                <td> {item.address.geolocation.long}</td>
-                <td> {item.address.street}</td>
-                <td> {item.address.zipcode}</td>
-                </tr>
-            </>
-        )
-    })
+    const selector =
+        data.map((item, id) => {
+            //    console.log("name",item.name)
+            return (
+                <>
+                    <tr>
+                        <th scope="row"> {item.id}  </th>
+                        <td style={{fontFamily: "monospace"}}>  {item.email}</td>
+                        <td> {item.password}</td>
+                        <td  style={{fontFamily: "monospace"}}> {item.username}</td>
+                        <td>{item.address.city} </td>
+                        <td  style={{fontFamily: "monospace"}}> {item.address.number}</td>
+                        <td> {item.address.geolocation.lat}</td>
+                        <td  style={{fontFamily: "monospace"}}> {item.address.geolocation.long}</td>
+                        <td> {item.address.street}</td>
+                        <td  style={{fontFamily: "monospace"}}> {item.address.zipcode}</td>
+                        <td> {item.name.firstname}</td>
+                        <td  style={{fontFamily: "monospace"}}> {item.name.lastname}</td>
+                    </tr>
+                </>
+            )
+        })
 
 
     return (
         <div>
 
-<table className='table table-dark'>
-                            <thead>
-                                <tr>
-                                <th scope='col' style={{color:"blueviolet"}}>id</th>
-                                    <th scope='col' style={{color:"blueviolet"}}>email</th>
-                                    <th scope='col' style={{color:"blueviolet"}} >password</th>
-                                    <th scope='col' style={{color:"blueviolet"}}>username</th>
-                                    <th scope='col' style={{color:"blueviolet"}}>city</th>
-                                    <th scope='col' style={{color:"blueviolet"}}>number</th>
-                                    <th scope='col' style={{color:"blueviolet"}}>latitude</th>
-                                    <th scope='col' style={{color:"blueviolet"}}>longitude</th>
-
-                                    <th scope='col' style={{color:"blueviolet"}}>street</th>
-                                    <th scope='col' style={{color:"blueviolet"}}>zipcode</th>
-
-
-
-                                </tr>
-                            </thead>
-                            <tbody>{selector}</tbody>
-                        </table>
+            <table className='table table-dark'>
+                <thead>
+                    <tr>
+                        <th scope='col' style={{ color: "blueviolet" }}>id</th>
+                        <th scope='col' style={{ color: "green" }}>email</th>
+                        <th scope='col' style={{ color: "blueviolet" }} >password</th>
+                        <th scope='col' style={{ color: "green" }}>username</th>
+                        <th scope='col' style={{ color: "blueviolet" }}>city</th>
+                        <th scope='col' style={{ color: "green" }}>number</th>
+                        <th scope='col' style={{ color: "blueviolet" }}>latitude</th>
+                        <th scope='col' style={{ color: "green" }}>longitude</th>
+                        <th scope='col' style={{ color: "blueviolet" }}>street</th>
+                        <th scope='col' style={{ color: "green" }}>zipcode</th>
+                        <th scope='col' style={{ color: "blueviolet" }}>firstname</th>
+                        <th scope='col' style={{ color: "green" }}>lastname</th>
+                    </tr>
+                </thead>
+                <tbody>{selector}</tbody>
+            </table>
 
 
-          
+
         </div>
     )
 }
